c4fe68752c23dcc8e90a6042dbfa11cb4b9db79e
brandon-rowe <brandon@cyntax.org>
2021-05-14 16:29:28 -0400

    Remove Test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -n "Enter your URL (your_url.com no 'https') \"url\" "
-read user_url
-echo "The value of \"url\" is now $user_url."
-
-
-mkdir ~/../srv/www/
-mkdir ~/../srv/www/$user_url
