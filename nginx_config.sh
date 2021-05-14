eb3420c2369f736503ca6b03df29c984c5fafcf1
brandon-rowe <brandon@cyntax.org>
2021-05-14 19:54:15 +0000

    Transfer from server

---
+++ nginx_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "#######################################################################"
+echo "Hello 007"
+echo "Q is Configuring Nginx for You"
+echo "#######################################################################"
+echo "Make www root folder for server"
+mkdir ~/../srv/www/
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo -n "Enter your URL (your_url.com no 'https') \"url\" "
+read user_url
+echo "The value of \"url\" is now $user_url."
