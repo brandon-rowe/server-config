14b28080ddd8922eef082f57c7b8cdd5da4f978b
brandon-rowe <brandon@cyntax.org>
2021-05-26 18:49:19 +0000

    Final Commit

--- certbot_config.sh
+++ certbot_config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo 
-read -p 'Have you installed certbot already? (Y)es or (N)o' answer
-if [ $answer=='Y' ]    
-then 
-    sudo certbot --nginx
-else
-    sudo snap install core
-    sudo snap refresh core
-    sudo apt-get remove certbot
-    sudo snap install --classic certbot
-    sudo ln -s /snap/bin/certbot /usr/bin/certbot
-    sudo certbot --nginx
+
+read -p 'Have you installed certbot already? (Y)es or (N)o: ' answer
+if [ $answer == 'Y']
+	then
+		sudo certbot --nginx
+	else
+		sudo snap install core
+		sudo snap refresh core
+		sudo apt-get remove certbot
+		sudo snap install --classic certbot
+		sudo ln -s /snap/bin/certbot /usr/bin/certbot
+		sudo certbot --nginx
 fi
